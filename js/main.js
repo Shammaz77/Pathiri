0f9fab515da8ac449baf4bedf3198ea605f98434
shammasnhd@gmail.com <Shammaz77>
2023-09-22 22:33:53 +0530

    added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const testimonials = document.querySelectorAll(".testimonial");
     let currentIndex = 0;
@@ -28,3 +32,51 @@ document.addEventListener("DOMContentLoaded", function () {
 function toggleCartt(){
     document.querySelector('.sidebar').classList.toggle('open-cart');
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let selectedImageContainer = null;
+
+    function selectImage(imageContainerId) {
+        if (selectedImageContainer) {
+            selectedImageContainer.classList.remove("selected");
+        }
+
+        selectedImageContainer = document.getElementById(imageContainerId);
+        selectedImageContainer.classList.add("selected");
+    }
+
+    function submitSelectedImage() {
+        if (selectedImageContainer) {
+            const imageAlt = selectedImageContainer.querySelector("img").alt;
+            alert(`You selected: ${imageAlt}`);
+            // Perform any other actions you need with the selected image here.
+        } else {
+            alert("Please select an image first.");
+        }
+    }
+
+    // Automatically select the first image when the page loads
+    window.addEventListener("load", function() {
+        selectImage("image1-container");
+    });
+
+    document.getElementById("image1-container").addEventListener("click", function() {
+        selectImage("image1-container");
+    });
+
+    document.getElementById("image2-container").addEventListener("click", function() {
+        selectImage("image2-container");
+    });
